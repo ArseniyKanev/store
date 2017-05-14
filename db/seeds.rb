e9fc6a692e57e0ee516497436289dfdf9ae66b35
Arseniy Kanev <arseniy.kan@gmail.com>
2017-05-14 04:45:05 +0300

    basic auth, product

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times { Product.create(
+  title: Faker::Commerce.product_name,
+  description: Faker::Lorem.sentence,
+  price: Faker::Commerce.price
+)}
